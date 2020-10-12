128463b599a1334b28edb7762cf78cdefe6bfa27
Hanqing1996 <zhq199622@163.com>
2020-10-12 19:53:38 +0800

    实现 magicRender 方法，用于解析节点

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
-const profile = (
-    <div>
-    <img src="avatar.png" className="profile" />
-    <h3>{[user.firstName, user.lastName].join(' ')}</h3>
-</div>
-);
+/**
+ * 由内至外，
+ * 解析 h1 节点,调用一次 magicRender
+ * 解析 div 节点，再调用一次 magicRender
+ *
+ * @param tag
+ * @param attrs
+ * @param children
+ * @returns {{children: *[], tag: *, attrs: *}}
+ */
+function magicRender( tag, attrs, ...children ) {
+    
+    console.log(arguments);
+    
+    
+    return {
+        tag,
+        attrs,
+        children
+    }
+}
+
+export const element = (
+        <div>
+            <h1>Hello, world!</h1>
+        </div>
+    )
+
+// console.log(element);
